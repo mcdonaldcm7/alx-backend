e886e49cd29dfe26fdf85bf874b45f53a60afc8c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-26 22:25:40 +0100

    Fixed type annotation

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -25,7 +25,7 @@ import math
 from typing import List
 
 
-def index_range(page: int, page_size: int) -> tuple(int, int):
+def index_range(page: int, page_size: int) -> type(int):
     """
     Returns a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
